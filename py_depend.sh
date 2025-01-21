83e58e54ba4503a33e368c4030769f70f1aaf432
WaterHyacinthInNANHU <mingxuanyan99@outlook.com>
2025-01-20 17:00:58 -0800

    dependencies installation script

---
+++ py_depend.sh
@@ -0,0 +1,16 @@
+
+# for cuda 12.4
+pip3 install torch torchvision torchaudio
+pip install -U openmim
+mim install mmengine
+mim install "mmcv==2.1.0"
+mim install 'mmdet>=3.0.0'
+pip install mayavi
+pip install timm
+pip install scikit-video
+pip install einops
+pip install scikit-image
+pip install nuscenes-devkit
+pip install -U diffusers accelerate transformers
+pip install pyvirtualdisplay
+pip install configobj
